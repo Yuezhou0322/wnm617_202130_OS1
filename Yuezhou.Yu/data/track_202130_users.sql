0957d98bb1912e2de3c1214ac25d7fe8f076dc30
Yuezhou Yu <yuyuezhou0322@gmail.com>
2021-05-19 04:20:36 -0700

    Module 8

---
+++ Yuezhou.Yu/data/track_202130_users.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `track_202130_users` (
+`id` INT NULL,
+`name` VARCHAR(MAX) NULL,
+`username` VARCHAR(MAX) NULL,
+`email` VARCHAR(MAX) NULL,
+`password` VARCHAR(MAX) NULL,
+`img` VARCHAR(MAX) NULL,
+`date_create` VARCHAR(MAX) NULL
+);
+
+INSERT INTO track_202130_users VALUES
+(1,'Wendi Nielsen','user1','user1@gmail.com','md5(pass)','https://via.placeholder.com/400/758/fff/?text=user1','2020-11-10 11:20:48'),
+(2,'Peggy Day','user2','user2@gmail.com','md5(pass)','https://via.placeholder.com/400/807/fff/?text=user2','2020-06-18 05:04:31'),
+(3,'Reva Barton','user3','user3@gmail.com','md5(pass)','https://via.placeholder.com/400/828/fff/?text=user3','2020-10-22 04:25:38'),
+(4,'Buckley Macdonald','user4','user4@gmail.com','md5(pass)','https://via.placeholder.com/400/723/fff/?text=user4','2020-05-09 09:40:06'),
+(5,'Hilary Puckett','user5','user5@gmail.com','md5(pass)','https://via.placeholder.com/400/736/fff/?text=user5','2020-08-19 05:04:59'),
+(6,'Jarvis Calhoun','user6','user6@gmail.com','md5(pass)','https://via.placeholder.com/400/780/fff/?text=user6','2020-07-02 12:25:41'),
+(7,'Boyle Ortiz','user7','user7@gmail.com','md5(pass)','https://via.placeholder.com/400/778/fff/?text=user7','2020-07-30 05:45:27'),
+(8,'Janie Jensen','user8','user8@gmail.com','md5(pass)','https://via.placeholder.com/400/783/fff/?text=user8','2021-04-16 09:08:43'),
+(9,'Mari Langley','user9','user9@gmail.com','md5(pass)','https://via.placeholder.com/400/723/fff/?text=user9','2020-02-15 01:32:42'),
+(10,'Nettie Franklin','user10','user10@gmail.com','md5(pass)','https://via.placeholder.com/400/720/fff/?text=user10','2020-03-04 07:45:55');
